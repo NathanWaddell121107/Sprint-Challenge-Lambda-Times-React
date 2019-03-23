49294e2917abfd3ed226425e32c2c0511eb1c8c7
NathanWaddell121107 <waddellnathan1211@gmail.com>
2019-03-23 12:44:01 -0500

    All functioning now. Also added more PropTypes.

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
 const Card = props => {
   return (
@@ -22,6 +22,6 @@ Card.propTypes = {
     img: PropTypes.string,
     author: PropTypes.string
   })
-}
+};
 
 export default Card;
--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -45,9 +45,9 @@ export default class Content extends Component {
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
 
-    this.state.selected === "all"
-      ? this.state.cards
-      : this.state.filter(card => card.tab === this.state.selected);
+    if (this.state.selected !== "all") {
+      return this.state.cards.filter(card => card.tab === this.state.selected);
+    }
     return this.state.cards;
   };
 
